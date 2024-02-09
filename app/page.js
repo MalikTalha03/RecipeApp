b4bd665b47d310eaa2abad692c7b28c6d2811c6f
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-09 21:22:51 +0500

    Add Nav component and update home page layout

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Image from "next/image";
+import Nav from "@/components/Nav";
 
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <Nav />
+      <div className="flex flex-col items-center justify-center h-screen gap-4">
+        <h1 className="text-5xl font-bold">Welcome to Next.js</h1>
+      </div>
     </div>
   );
 }
