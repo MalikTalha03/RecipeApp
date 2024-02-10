9fb332f31f2aea082490517edf79812d4951d475
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-10 11:26:13 +0500

    Add search input field to Home page

--- app/page.js
+++ app/page.js
@@ -6,6 +6,11 @@ export default function Home() {
       <h1 className="text-4xl font-bold">Discover & Share new Recipes</h1>
       <p className="text-lg mt-4">Tastebite is a platform for foodies to share 
       their favorite recipes with the world.</p>
+      <input 
+        type="search"
+        placeholder="Enter Recipe Name"
+        className="p-2 rounded-lg w-1/2 border-2 border-gray-300 mt-4"
+      />
     </section>
   );
 }
