537eb6906c1dd67d9cbcca3f1d8296dd8473f2d1
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-11 15:42:30 +0500

    Add delete and edit functionality to recipe page

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,12 +2,13 @@
 import { useSession } from "next-auth/react";
 import Profile from "@components/Profile";
 import { useState, useEffect } from "react";
+import { useRouter } from "next/navigation";
 
 const Page = () => {
   const { data: session } = useSession();
   const [recipes, setRecipes] = useState([]);
   const [loading, setLoading] = useState(true);
-  console.log(session);
+  const router = useRouter();
   const getRecipes = async () => {
     try {
       const res = await fetch("/api/user/" + session?.user.id);
@@ -21,8 +22,21 @@ const Page = () => {
   useEffect(() => {
     getRecipes();
   }, [session?.user.id]);
-  const handleDelete = () => {}
-  const handleEdit = () => {}
+  const handleDelete = (recipe) => {
+    if (!confirm("Are you sure you want to delete this recipe?")) return;
+    fetch("/api/recipe/" + recipe._id, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        const newRecipes = recipes.filter((recipe) => recipe._id !== data._id);
+        setRecipes(newRecipes);
+      })
+      .catch((error) => alert(error));
+  };
+  const handleEdit = (recipe) => {
+    router.push("/update-recipe?id=" + recipe._id);
+  };
   return (
     <section className="w-full max-w-4xl mx-auto flex flex-col items-center justify-center mt-8 gap-4">
       {recipes && recipes.length > 0 ? (
