3f3430667276d6ac81d8cab4e8ff8894f2204463
MalikTalha03 <mtnaqshbandi2003@gmail.com>
2024-02-11 14:31:06 +0500

    Add loading state to Feed component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -4,12 +4,13 @@ import { useEffect, useState } from "react";
 
 const Feed = () => {
   const [recipes, setRecipes] = useState([]);
+  const [loading, setLoading] = useState(true);
   const getRecipes = async () => {
     try {
       const res = await fetch("/api/recipe");
       const data = await res.json();
-      console.log(data);
-      setRecipes(res);
+      setRecipes(data);
+      setLoading(false);
     } catch (error) {
       alert(error);
     }
@@ -18,12 +19,14 @@ const Feed = () => {
     getRecipes();
   }, []);
   return (
-    <section className="w-full max-w-4xl mx-auto">
-      <h2 className="text-2xl font-bold text-center">Feed</h2>
-      {recipes &&
+    <section className="w-full max-w-4xl mx-auto flex  justify-center mt-8 gap-4 mb-16">
+      {recipes && recipes.length > 0 ? (
         recipes.map((recipe, index) => (
           <RecipeCard key={index} recipe={recipe} />
-        ))}
+        ))
+      ) : (
+        <p className="text-center">{loading ? "Loading..." : "No Recipes Found"}</p>
+      )}
     </section>
   );
 };
